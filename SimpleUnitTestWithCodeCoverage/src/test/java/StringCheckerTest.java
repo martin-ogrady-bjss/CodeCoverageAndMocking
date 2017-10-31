76776ff521afa19d57f519b06790ca6aae80a649
Martin O'Grady <martinog2@gmail.com>
2017-10-30 22:24:23 -0400

    Added SimpleUnitTestWithCodeCoverage project

---
+++ SimpleUnitTestWithCodeCoverage/src/test/java/StringCheckerTest.java
@@ -0,0 +1,29 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StringCheckerTest
+{
+	@Test
+	public void testGoodString()
+	{
+		StringChecker checker = new StringChecker();
+		assertTrue(checker.isGoodString("abc"));
+	}
+
+
+	@Test
+	public void testStringWithSpace()
+	{
+		StringChecker checker = new StringChecker();
+		assertFalse(checker.isGoodString("abc def"));
+	}
+
+	
+//	@Test
+//	public void testNullString()
+//	{
+//		StringChecker checker = new StringChecker();
+//		assertFalse(checker.isGoodString(null));
+//	}
+}
